968adcbcf6ee054f165a642d7492915cb27d682d
Vaibhav <vaibhavatara@example.com>
2020-04-14 18:48:30 +0530

    added linechart 1.0

--- app/src/main/java/com/example/mongoconnect/LineChartVis.java
+++ app/src/main/java/com/example/mongoconnect/LineChartVis.java
@@ -75,25 +75,6 @@ public class LineChartVis extends AppCompatActivity {
         }
 //######################################################################################
 
-        /*ArrayList<Entry> yValues = new ArrayList<>();
-
-        yValues.add(new Entry(1,60f));
-        yValues.add(new Entry(2,50f));
-        yValues.add(new Entry(3,70f));
-        yValues.add(new Entry(4,30f));
-        yValues.add(new Entry(5,80f));
-        yValues.add(new Entry(6,40f));
-        yValues.add(new Entry(7,50f));
-
-        LineDataSet set1 = new LineDataSet(yValues,"Data Set1");
-
-        set1.setFillAlpha(110);
-
-        ArrayList<ILineDataSet> dataset = new ArrayList<>();
-        dataset.add(set1);
-
-        LineData data = new LineData(dataset);
-        lineChart.setData(data);*/
 
                 plot();
 
